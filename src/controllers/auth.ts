ebe2069c9eb49116cc54a64e8493f80570268858
chetan <chetanbhosale810@gmail.com>
2024-02-11 21:39:50 +0530

    added two more controllers into authentication 1) changePassword 2) forgotPassword

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,5 +1,11 @@
-import {  Request, Response, Express } from "express";
-import { SignUpSchema, loginSchema, otpEmailValidaton } from "../schemas/auth";
+import { Request, Response, Express } from "express";
+import {
+  SignUpSchema,
+  changePasswordSchema,
+  forgotPasswordSchema,
+  loginSchema,
+  otpEmailValidaton,
+} from "../schemas/auth";
 import { prisma } from "../db/db";
 import { BadRequestException } from "../exceptions/bad-request";
 import { ErrorCode } from "../exceptions/root";
@@ -9,10 +15,7 @@ import { JWT_SECRET } from "../secreat";
 import sendMail from "../services/node-email";
 import { MySession } from "..";
 
-export const signup = async (
-  req: Request,
-  res: Response,
-) => {
+export const signup = async (req: Request, res: Response) => {
   const data = SignUpSchema.parse(req.body);
   const { email, password, otp } = data;
 
@@ -36,7 +39,10 @@ export const signup = async (
   });
 
   if (!matchedOtp) {
-    throw new BadRequestException("Otp is incorrect!", ErrorCode.OTP_IS_INCORRECT);
+    throw new BadRequestException(
+      "Otp is incorrect!",
+      ErrorCode.OTP_IS_INCORRECT
+    );
   }
 
   User = await prisma.user.create({
@@ -49,27 +55,28 @@ export const signup = async (
   res.status(201).json({
     success: true,
     data: User,
+    message: "signed up successfully!",
   });
 };
 
-export const sendOtp = async (
-  req: Request,
-  res: Response,
-) => {
+export const sendOtp = async (req: Request, res: Response) => {
   const { email } = otpEmailValidaton.parse(req.body);
 
-  let user = await prisma.user.findUnique({where : {email}})
+  let user = await prisma.user.findUnique({ where: { email } });
 
-  if(user){
-    throw new BadRequestException('User already exists',ErrorCode.USER_ALREADY_EXITS)
+  if (user) {
+    throw new BadRequestException(
+      "User already exists",
+      ErrorCode.USER_ALREADY_EXITS
+    );
   }
 
   let randomNumber: number = 0;
   while (randomNumber <= 99999) {
     randomNumber = Math.floor(100000 + Math.random() * 900000);
   }
-  
-  console.log(randomNumber)
+
+  console.log(randomNumber);
 
   const newOtp = {
     email,
@@ -87,10 +94,7 @@ export const sendOtp = async (
   });
 };
 
-export const login = async (
-  req: Request,
-  res: Response,
-) => {
+export const login = async (req: Request, res: Response) => {
   const { email, password } = loginSchema.parse(req.body);
 
   const user = await prisma.user.findUnique({ where: { email } });
@@ -124,6 +128,102 @@ export const login = async (
         success: true,
         data: user,
         token: token,
+        message: "logged in!",
       });
+  }
+};
+
+export const changePassword = async (req: Request, res: Response) => {
+  const { email, oldPassword, newPassword, otp } = changePasswordSchema.parse(
+    req.body
+  );
+
+  let user = await prisma.user.findUnique({ where: { email } });
+
+  if (!user) {
+    throw new BadRequestException("User not found!", ErrorCode.USER_NOT_FOUND);
+  } else {
+    const storedOtps = (req.session as MySession).otps || [];
+
+    const matchedOtp = storedOtps.find((storedOtp) => {
+      return (
+        email === storedOtp.email &&
+        otp === storedOtp.value &&
+        Date.now() <= storedOtp.expiresAt
+      );
+    });
+
+    if (!matchedOtp) {
+      throw new BadRequestException(
+        "Otp is incorrect!",
+        ErrorCode.OTP_IS_INCORRECT
+      );
+    }
+
+    const checkPass = compareSync(oldPassword, user.password);
+
+    if (!checkPass) {
+      throw new BadRequestException(
+        "Password doest match",
+        ErrorCode.INCORRECT_PASSWORD
+      );
+    }
+
+    user.password = newPassword;
+
+    let data = await prisma.user.update({
+      where: { email },
+      data: {
+        ...user,
+      },
+    });
+
+    res.status(201).json({
+      success: true,
+      data: data,
+      message: "user updated successfully!",
+    });
+  }
+};
+
+export const forgotPassword = async (req: Request, res: Response) => {
+  const { email, otp, newPassword } = forgotPasswordSchema.parse(req.body);
+
+  let user = await prisma.user.findUnique({ where: { email } });
+
+  if (!user) {
+    throw new BadRequestException("User not found!", ErrorCode.USER_NOT_FOUND);
+  } else {
+    const storedOtps = (req.session as MySession).otps || [];
+
+    const matchedOtp = storedOtps.find((storedOtp) => {
+      return (
+        email === storedOtp.email &&
+        otp === storedOtp.value &&
+        Date.now() <= storedOtp.expiresAt
+      );
+    });
+
+    if (!matchedOtp) {
+      throw new BadRequestException(
+        "Otp is incorrect!",
+        ErrorCode.OTP_IS_INCORRECT
+      );
+    }
+
+    user.password = newPassword;
+
+    let data = await prisma.user.update({
+      where: { email },
+      data: {
+        ...user,
+      },
+    });
+
+    res.status(201).json({
+      success: true,
+      data: data,
+      message: "user updated successfully!",
+    });
   }
 };
--- src/schemas/auth.ts
+++ src/schemas/auth.ts
@@ -15,3 +15,16 @@ export const loginSchema = z.object({
 export const otpEmailValidaton = z.object({
     email : z.string().email().toLowerCase(),
 })
+
+export const changePasswordSchema = z.object({
+     email : z.string().email().toLowerCase(),
+     oldPassword : z.string().min(6),
+     newPassword: z.string().min(6),
+     otp : z.string().min(6)
+})
+
+export const forgotPasswordSchema = z.object({
+    email : z.string().email().toLowerCase(),
+    newPassword : z.string().min(6),
+    otp : z.string().min(6)
+})
